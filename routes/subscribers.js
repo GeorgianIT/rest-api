c674b6107b1ffe23e195fadcba205f5d0c676b6c
GeorgianIT <iulian.tudor2405@gmail.com>
2023-03-09 14:39:38 +0200

    Making routes work, and call them in visual studio using Rest Client Extension

---
+++ routes/subscribers.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+
+//Getting all
+router.get('/', (req, res) => {
+    res.send('Hello World')
+})
+//Getting One
+router.get('/:id', (req, res) => {
+    res.send(req.params.id)
+})
+//Creating One
+router.post('/', (req, res) => {
+    
+})
+//Updating One
+router.patch('/', (req, res) => {
+    
+})
+//Deleting one
+router.delete('/:id', (req, res) => {
+    
+})
+
+module.exports = router
